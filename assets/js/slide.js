54a78e7a489dbed93be26ae3c783e90625950a33
zainal3096 <zainal.makhasin@students.amikom.ac.id>
2018-06-02 16:50:48 +0700

    update kabeh

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -25,7 +25,7 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active_slide";
-    setTimeout(showSlides, 4000); // Change image every 2 seconds
+    setTimeout(showSlides, 4000);
 }
 
 function plusDivs(n) {
